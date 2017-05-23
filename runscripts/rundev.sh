67000c6150146d766fb694768d94468ede734386
Murali Shankar <mshankar@slac.stanford.edu>
2017-05-23 15:32:20 -0700

    Start under supervisord

--- runscripts/rundev.sh
+++ runscripts/rundev.sh
@@ -11,9 +11,22 @@ source /reg/g/psdm/bin/conda_setup ""
 # Assume that we are running the in root folder of this package
 PRNT_DIR=`dirname $PWD`
 G_PRNT_DIR=`dirname $PRNT_DIR`;
+GG_PRNT_DIR=`dirname $G_PRNT_DIR`;
+GGG_PRNT_DIR=`dirname $GG_PRNT_DIR`;
+
+
+CONFIG_FILE="${GGG_PRNT_DIR}/appdata/pcds_expresults/pcds_expresults.sh"
+echo "External config file is ${CONFIG_FILE}"
+
+if [[ -f "${CONFIG_FILE}" ]]
+then
+   echo "Sourcing deployment specific configuration from ${CONFIG_FILE}"
+   source "${CONFIG_FILE}"
+fi
+
 
 # Pick up psdmauth for the test deployment
-export PYTHONPATH="${PRNT_DIR}/psdmauth/src:${PYTHONPATH}"
+export PYTHONPATH="${GG_PRNT_DIR}/psdmauth/src:${PYTHONPATH}"
 echo "Using psdmauth from ${PYTHONPATH}"
 
 export ACCESS_LOG_FORMAT='%(h)s %(l)s %({REMOTE_USER}i)s %(t)s "%(r)s" %(s)s %(b)s %(D)s'
