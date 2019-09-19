ea6c576c56ad3b0329a0eac3ccca08a0b240dfd1
Murali Shankar <mshankar@slac.stanford.edu>
2019-09-19 12:01:23 -0700

    Clean up logging

--- pages.py
+++ pages.py
@@ -8,6 +8,8 @@ __author__ = 'mshankar@slac.stanford.edu'
 
 pages_blueprint = Blueprint('pages_api', __name__)
 mimetypes.init()
+logger = logging.getLogger(__name__)
+
 
 @pages_blueprint.route("/<instrument_name>/<experiment_id>-<experiment_name>", methods=["GET"])
 @pages_blueprint.route("/<instrument_name>/<experiment_id>-<experiment_name>/summary.html", methods=["GET"])
@@ -17,20 +19,23 @@ def mainSummary(instrument_name, experiment_id, experiment_name):
     instrument_name = instrument_name.lower()
     # First check to see if the run summary folder exists for the experiment
     expResultsFolder = os.path.join(EXP_RESULTS_FOLDER, instrument_name, experiment_name, "stats", "summary")
-    current_app.logger.debug("Looking for summary results for experiment {} in folder {}".format(experiment_name, expResultsFolder))
+    logger.debug("Looking for summary results for experiment {} in folder {}".format(experiment_name, expResultsFolder))
 
     if not os.path.exists(expResultsFolder):
+        logger.debug("Cannot find the folder %s", expResultsFolder)
         return Response("There are no summary stats for {}".format(experiment_name), mimetype='text/html')
 
+    logger.debug("Found the path %s", expResultsFolder)
     if not os.path.isdir(expResultsFolder):
         return Response("The summary stats path {} is not a folder for {}".format(expResultsFolder, experiment_name), mimetype='text/html')
 
+    logger.debug("Found the folder %s", expResultsFolder)
     if os.path.exists(os.path.join(expResultsFolder, "report.html")):
-        current_app.logger.debug("Found an report.html")
+        logger.debug("Found an report.html")
         return send_file(os.path.join(expResultsFolder, "report.html"), mimetype='text/html')
 
     if os.path.exists(os.path.join(expResultsFolder, "experiment", "report.html")):
-        current_app.logger.debug("Found an experiment report.html")
+        logger.debug("Found an experiment report.html")
         return send_file(os.path.join(expResultsFolder, "experiment", "report.html"), mimetype='text/html')
     # The default is to send a list of folders as links.
     links = []
@@ -38,7 +43,7 @@ def mainSummary(instrument_name, experiment_id, experiment_name):
         if os.path.isdir(os.path.join(expResultsFolder, folderName)) and os.path.exists(os.path.join(expResultsFolder, folderName, "report.html")):
             links.append(folderName)
     else:
-        current_app.logger.debug("'{}' does not exist?".format(os.path.join(expResultsFolder, folderName, "report.html")))
+        logger.debug("'{}' does not exist?".format(os.path.join(expResultsFolder, folderName, "report.html")))
 
     return render_template("expsummary.html",
                            links=links,
@@ -54,7 +59,7 @@ def otherPages(instrument_name, experiment_id, experiment_name, page):
     instrument_name = instrument_name.lower()
     # First check to see if the run summary folder exists for the experiment
     expResultsPage = os.path.join(EXP_RESULTS_FOLDER, instrument_name, experiment_name, "stats", "summary", page)
-    current_app.logger.debug("Looking for page {} for experiment {} in folder {}".format(page, experiment_name, expResultsPage))
+    logger.debug("Looking for page {} for experiment {} in folder {}".format(page, experiment_name, expResultsPage))
 
     if not os.path.exists(expResultsPage):
         abort(404)
