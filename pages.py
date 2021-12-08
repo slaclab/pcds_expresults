442b4447ef0d082b3a0fda0614e7d02a6639c8b1
Murali Shankar <mshankar@slac.stanford.edu>
2021-12-08 14:38:09 -0800

    Sort the folders

--- pages.py
+++ pages.py
@@ -90,4 +90,4 @@ def svc_walk_stats_summary_tree(instrument, experiment):
     for (root, dirs, files) in os.walk(expResultsFolder, followlinks=False):
         if "report.html" in files:
             fldrs.append({"root": root[len(expResultsFolder)+1:], "hasReport": True})
-    return jsonify({'success': True, 'value': fldrs})
+    return jsonify({'success': True, 'value': sorted(fldrs, key=lambda x: x["root"])})
